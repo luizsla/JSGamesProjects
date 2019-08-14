b4a49366f2ea46d1ee3e5a1d239d4ef8fba9ac2d
Luiz Eduardo Amorim <eduardogbi10@yahoo.com.br>
2019-08-14 19:57:19 -0300

    m

---
+++ Guessing Game/app.js
@@ -0,0 +1,20 @@
+(function() {
+    var questions = [
+      {
+        title: '',
+        options: [
+
+        ],
+
+      },
+      {
+
+      },
+      {
+
+      },
+    ];
+    var response = prompt('Hello').trim();
+
+    console.log(response);
+})();
